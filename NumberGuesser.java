a5270f8194f7e435933d4ab175f96bbf428066fd
janblocker2003 <janblocker2003@gmail.com>
2026-03-13 17:11:13 +0000

    almost working completely, small logic error with comp guesser

--- NumberGuesser.java
+++ NumberGuesser.java
@@ -24,11 +24,13 @@ public class NumberGuesser{
     				computerGuesser();
     			} // end else if
 			else if (response.equals("0")){
-        			System.out.println("Goodbye!");
+        			System.out.println(" ");
+				System.out.println("Goodbye!");
         			keepGoing = false;
     			} // end else if
 			else {
-        			System.out.println("Sorry. I didn't understand");
+        			System.out.println(" ");
+				System.out.println("Sorry. I didn't understand");
     			} // end else
   		} // end while
 	} // end constructor
@@ -36,11 +38,13 @@ public class NumberGuesser{
 
  	public String menu(){
 		//prints a menu, returns a single character string 0, 1, or 2
-     		System.out.println("Select an option");
+		System.out.println(" ");
+     		System.out.println("Select an option (0-2)");
      		System.out.println("0) Quit");
      		System.out.println("1) Human Guesser");
      		System.out.println("2) Computer Guesser");
      		String response = input.nextLine();
+		System.out.println(" ");
      		return response;
    	} // end menu
 
@@ -52,28 +56,31 @@ public class NumberGuesser{
   		while (keepGoing == true) {
         		//ask user for a number and put into -> new String guess
         		turns++;
-			System.out.println (turns + ") Please enter a number: ");
+			System.out.println (turns + ") Please enter a number 1-100: ");
         		String guess = input.nextLine();
-        		
+        		System.out.println(" ");
 			try {
                         	int intGuess = Integer.parseInt(guess);
-                        } // end try
-        			if (intGuess < correct) {
+				System.out.println(" ");
+				
+				if (intGuess > 100) {
+                                        System.out.println ("The number has to be between 1-100!");
+                                } // end if
+                                else if (intGuess < 1) {
+                                        System.out.println ("The number has to be between 1-100!");
+                                } //end else if
+				else if (intGuess < correct) {
             				System.out.println ("Too low");
-        			} // end if
+        			} // end else if
 				else if (intGuess > correct) {
             				System.out.println ("Too high");
         			} //end else if
-				else if (intGuess == correct) {
+				else {
             				System.out.println ("You guessed correct!");
             				keepGoing = false;
-        			} // end else if
-        			else if (intGuess > 100) {
-            				System.out.println ("The number has to be between 1-100!");
-				} // end else if
-        			else (intGuess < 1) {
-                                	System.out.println ("The number has to be between 1-100!");
+					System.out.println(" ");
 				} //end else
+			} // end try
 			catch (NumberFormatException e) {
                                 System.out.println("That is not a number!");
 			} // end catch
@@ -90,18 +97,26 @@ public class NumberGuesser{
 			System.out.println (turns + ") I guess " + compGuess);
     			System.out.println ("Too (H)igh, too (L)ow, or (C)orrect? ");
     			String feedback = input.nextLine();
+			System.out.println(" ");
     			feedback = feedback.toUpperCase();
+			if (changeBy < 1) {
+				changeBy = 1;
+			} // end if
+
     			if (feedback.equals("H")) {
       				compGuess -= changeBy;
       				changeBy /= 2;
+				changeBy += 1;
     			} // end if
     			else if (feedback.equals("L")) {
       				compGuess += changeBy;
       				changeBy /= 2;
+				changeBy += 1;
     			} // end else if
     			else if (feedback.equals("C")) {
       				System.out.println ("Yippee!");
       				keepGoing = false;
+				System.out.println(" ");
     			} //end else if
     			else {
       				System.out.println("Sorry. I didn't understand");
